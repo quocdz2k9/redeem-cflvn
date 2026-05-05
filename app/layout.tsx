f90b7a1c92906d4e7bd988d6cba95d024b5bde52
root <root@localhost.localdomain>
2026-05-05 04:22:21 +0000

    Sửa lỗi date và cập nhật giao diện

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,33 @@ import { Toaster } from "@/components/ui/sonner";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "CFL Redeemer",
-  description: "Hệ thống quản lý và kích hoạt mã quà tặng tự động",
+  metadataBase: new URL(
+    process.env.VERCEL_URL 
+      ? `https://${process.env.VERCEL_URL}` 
+      : "http://localhost:3000"
+  ),
+  title: "Nhập Code Auto Hoàn Toàn Miễn Phí | CROSSFIRE: LEGENDS",
+  description: "Hệ thống quản lý và kích hoạt mã quà tặng tự động. Crossfire: Legends – huyền thoại FPS chính thức ra mắt tại Việt Nam!",
+  keywords: "nhập code cfl, auto code cfl, giftcode crossfire legends, code cfl 2026",
+  icons: {
+    icon: "https://cdn-mainsite-aka.vnggames.com/products/cfl/favicon.png",
+    shortcut: "https://cdn-mainsite-aka.vnggames.com/products/cfl/favicon.png",
+  },
+  openGraph: {
+    title: "CROSSFIRE: LEGENDS l HUYỀN THOẠI KHÔNG PHAI",
+    description: "Nhập code Auto hoàn toàn miễn phí. Trải nghiệm PvP kịch tính và kho vũ khí đỉnh cao. Gia nhập ngay nào!",
+    siteName: "CROSSFIRE: LEGENDS Auto Redeem",
+    images: [
+      {
+        url: "https://cdn-mainsite-aka.vnggames.com/products/cfl/mainsite/dist/assets/cfl-ms-25-header/images/bg.jpg",
+        width: 1200,
+        height: 630,
+        alt: "CFL Auto Code Preview",
+      },
+    ],
+    locale: "vi_VN",
+    type: "website",
+  },
 };
 
 export default function RootLayout({
@@ -18,7 +43,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="vi" suppressHydrationWarning className="h-full">
-      <head />
       <body className={`${inter.className} min-h-full flex flex-col antialiased`}>
         <ThemeProvider
           attribute="class"
